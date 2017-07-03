e312e597ef58521dd9a598c4b2446ea1a3349e30
DanielKnoll <knoll.dani@gmail.com>
2017-07-03 18:35:37 +0200

    Prints out the answers for the two first questions.

--- printing.py
+++ printing.py
@@ -1,2 +1,12 @@
+import reports
 
-# Printing functions
+file_name = "game_stat.txt"
+year = 2000
+
+print("How many games are in the file?", reports.count_games(file_name))
+print("Is there a game from a given year?", reports.decide(file_name, year))
+
+file_name = "game_stat.txt" # Delete from this line before push.
+is_simple_string_enough = False
+t = reports.read_file(file_name, is_simple_string_enough)
+print(t)
