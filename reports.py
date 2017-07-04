3bb61c3d03f6c3dcb8a7bc1f8c73bae463ec7adb
DanielKnoll <knoll.dani@gmail.com>
2017-07-04 14:57:05 +0200

    Finished functions for nice to have questions. Updated doc comments, added error handelings

--- reports.py
+++ reports.py
@@ -1,10 +1,10 @@
 
 
 def read_file(file_name, simple_string_is_enough):
-    """In the first version it was a file reading using with open, but I found out that some
-    functions can return the correct value from a simple string. So I made two separate reader.
-    One returns a string the other returns a dictionary. In order to do that I had to keep open
-    the file until the return command.
+    """In the first version the file reading used with open, but I found out that some functions
+    can return the correct value from a simple string. So I made two separate reader. One returns
+    a string the other returns a dictionary. In order to do that I had to keep open the file
+    until the return command. At least in this case there is only one line in the try.
     """
     try:
         file = open(file_name)
@@ -20,50 +20,53 @@ def read_file(file_name, simple_string_is_enough):
 
 
 def convert_file_to_string(file):
-    """For example for the first two questions I can simply return a string to work with."""
+    """For the first two questions for example f I can simply return a string to work with."""
     game_infos_string = file.read()
     return game_infos_string
 
 
 def convert_file_to_dict(file):
     """For the most of the questions the program will create a dictionary for each function call.
-    I did this to avoid global variables.
-    The game information categories will be the keys. Each category receives an empty list
-    through a for loop. The game informations will be appended to the corresponding dict keys.
-    The game will have it's information in the same indexes but in different lists.
-    I got rid of the \n with a slice command at the end of each line, and split the line at tabulators.
-    Since the information categories are in the same order as in the file I could append the right value to
-    the right dictionary key.
-    This will make easier to work with the same type of data, and reading the code.
+    Instead of doing the obvious and storing the file line by line in lists I figured I can use
+    simple commands if I store the columns in separate lists. I used the property names as keys
+    so it will be easier to read the code.
+    Since the information categories are in the same order as in the file I could append the right
+    value to the right dictionary key. The game informations will bein the same indexes but in
+    different lists.
+    I got rid of the '\n' with a slice command at the end of each line, and split the line at tabulators.
     """
-
-    game_infos = {}
-    info_categories = ["title", "copies sold", "release date", "genre", "publisher"]
+    game_infos_dict = {}
+    info_categories = ["titles", "copies sold lst", "release dates", "genres", "publishers"]
     for key in info_categories:
-        game_infos.update({key: []})
+        game_infos_dict.update({key: []})
 
+    line_counter_for_errors = 1
     for line in file:
+        line_counter_for_errors = check_errors_in_line(line, line_counter_for_errors)
         one_game = line[:-1].split("\t")
         for index, value in enumerate(one_game):
-            game_infos[info_categories[index]].append(value)
+            game_infos_dict[info_categories[index]].append(value)
 
-    return game_infos
+    return game_infos_dict
+
+
+def check_errors_in_line(line, line_counter_for_errors):
+    """Next task"""
+    pass
 
 
 def count_games(file_name):
-    """I can simply count the games by counting the EOL characters (\n) in the string since each game
-    is in a separate line. With different data structure the function has to be changed. But this way 
-    it uses less resources.
+    """With this data structure I can simply make the program count the games by counting the EOL
+    characters (\n) in the string since each game is in a separate line.
+    It uses less memory. With dictionary I could use len() on any list for the same result.
     """
-    simple_string_is_enough = True
-    game_infos_string = read_file(file_name, simple_string_is_enough)
+    game_infos_string = read_file(file_name, simple_string_is_enough=True)
     return game_infos_string.count("\n")
 
 
 def decide(file_name, year):
     """Another simple string operation, but I added a value check for the new argument."""
-    simple_string_is_enough = True
-    game_infos_string = read_file(file_name, simple_string_is_enough)
+    game_infos_string = read_file(file_name, simple_string_is_enough=True)
     try:
         int(year)
     except ValueError as err:
@@ -78,28 +81,94 @@ def decide(file_name, year):
 def get_latest(file_name):
     """First question that required more complex collection. But the dictionary made the function simple.
     The program finds the highest value in the list which contains all the release dates. The title is in
-    a different list ({"title": [?,?,?,...]}) but at the same index.
+    a different list ({"titles": [?,?,?,...],?,?}) but at the same index.
     I could have use a one-liner but it would have made the code harder to read.
     """
-    simple_string_is_enough = False
-    game_infos_dict = read_file(file_name, simple_string_is_enough)
-    latest_year = max(game_infos_dict["release date"])
-    index = game_infos_dict["release date"].index(latest_year)
-    return game_infos_dict["title"][index]
+    game_infos_dict = read_file(file_name, simple_string_is_enough=False)
+    latest_year = max(game_infos_dict["release dates"])
+    index = game_infos_dict["release dates"].index(latest_year)
+    return game_infos_dict["titles"][index]
 
 
 def count_by_genre(file_name, genre):
-    """Here I could use a simple list count thanks to the list with the same categories of values inside."""
-    simple_string_is_enough = False
-    game_infos_dict = read_file(file_name, simple_string_is_enough)
-    return game_infos_dict["genre"].count(genre)
+    """Here I could use a simple list count thanks to the list with the same properties inside."""
+    game_infos_dict = read_file(file_name, simple_string_is_enough=False)
+    return game_infos_dict["genres"].count(genre)
 
 
 def get_line_number_by_title(file_name, title):
-    """With dictionary I could have simply use the .index method for the title and return one value higher
-    to get the line number. But this can be solved with string methods also.
+    """With dictionary I could have simply use the index() method for the title and return one value higher
+    to get the line number. But this can be solved with string methods also without causing error. But the
+    assignment clearly instructed me to do so.
     """
-    simple_string_is_enough = True
-    game_infos_string = read_file(file_name, simple_string_is_enough)
-    length = game_infos_string.find(title)
+    game_infos_string = read_file(file_name, simple_string_is_enough=True)
+    try:
+        length = game_infos_string.find(title)
+        if length == -1:
+            raise ValueError
+    except ValueError:
+        return "This title is not in the file."
     return game_infos_string[:length].count("\n") + 1
+
+
+def sort_abc(file_name):
+    """I have used the updated bubble sort from Python SI1"""
+    game_infos_dict = read_file(file_name, simple_string_is_enough=False)
+    return bubble_sort(game_infos_dict["titles"])
+
+
+def bubble_sort(lists):
+    """Had to add the lower() to the comperation for get_genres for the right order.
+    So this function can only sort strings now.
+    """
+    for i in range(len(lists)-1, 0, -1):
+        swapped = False
+        for j in range(i):
+            if lists[j].lower() > lists[j+1].lower():
+                temp = lists[j]
+                lists[j] = lists[j+1]
+                lists[j+1] = temp
+                swapped = True
+        if not swapped:
+            break
+    return lists
+
+
+def get_genres(file_name):
+    """I already have all the genres in list, so I convert it to set to get rid of duplicates.
+    Then convert it back to list since it is the expected output. Then I sorted it with bubble
+    sort. The sorted() method would place RPG before Real-time strategy since "P" has a lower
+    hexadecimal value than "a".
+    """
+    game_infos_dict = read_file(file_name, simple_string_is_enough=False)
+    try:
+        sorted_non_redundant_genres_list = bubble_sort(list(set(game_infos_dict["genres"])))
+    except TypeError:
+        return "You changed the list to a non iterable type. Don't do that."
+    else:
+        return sorted_non_redundant_genres_list
+
+
+def when_was_top_sold_fps(file_name):
+    """I could use the advantage of the same types in one list solution here too. But the error handelings
+    makes it harder to read.
+    """
+    game_infos_dict = read_file(file_name, simple_string_is_enough=False)
+    top_sell = 0.0
+    game_index = 0
+    for index, value in enumerate(game_infos_dict["genres"]):
+        if value == "First-person shooter":
+            try:
+                game_sold = float(game_infos_dict["copies sold lst"][index])
+            except ValueError:
+                return "You have letters/symbols in the second value at line " + str(index + 1) + " in " + file_name
+
+            if game_sold > top_sell:
+                top_sell = game_sold
+                game_index = index
+    try:
+        year_of_top_sold_game = int(game_infos_dict["release dates"][game_index])
+    except ValueError:
+        return "You have letters/symbols in the third value at line " + str(game_index + 1) + " in " + file_name
+
+    return year_of_top_sold_game
