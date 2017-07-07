8eff2ff79e8653a99f1d7b25b570064749323886
DanielKnoll <knoll.dani@gmail.com>
2017-07-08 00:34:05 +0200

    For the first 7 questions

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,37 @@
+import sys
+import reports
 
-# Export functions
+
+def input_values():
+    file_name = "game_stat.txt"
+    title = "The Sims 2"
+    return file_name, title
+
+
+def get_answers(inputs):
+    """Returns a single string for the file exporter"""
+    file_name, title = inputs
+    answers = (reports.get_most_played(file_name) + "\n" +
+               str(reports.sum_sold(file_name)) + "\n" +
+               str(reports.get_selling_avg(file_name)) + "\n" +
+               str(reports.count_longest_title(file_name)) + "\n" +
+               str(reports.get_date_avg(file_name)) + "\n" +
+               str(reports.get_game(file_name, title)) + "\n" +
+               str(reports.count_grouped_by_genre(file_name)) + "\n")
+    return answers
+
+
+def export_answers(answers):
+    with open("export_answers.txt", "w") as f:
+        is_it_done = f.write(answers)
+    if is_it_done > 0:
+        print("You can find your answers in export_answers.txt")
+
+
+def main():
+    inputs = input_values()
+    answers = get_answers(inputs)
+    export_answers(answers)
+
+if __name__ == '__main__':
+    sys.exit(main())
--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,100 @@
 
-# Printing functions
+import sys
+import pprint
+import reports
+
+
+def input_values():
+    file_name = "game_stat.txt"
+    title = "The Sims 23"
+    return file_name, title
+
+
+def print_get_most_played(file_name):
+    question = "What is the title of the most played game?"
+    answer = reports.get_most_played(file_name)
+    my_pretty_print(question, answer)
+
+
+def print_sum_sold(file_name):
+    question = "How many copies have been sold total?"
+    answer = reports.sum_sold(file_name)
+    my_pretty_print(question, answer)
+
+
+def print_get_selling_avg(file_name):
+    question = "What is the average selling?"
+    answer = reports.get_selling_avg(file_name)
+    my_pretty_print(question, answer)
+
+
+def print_count_longest_title(file_name):
+    question = "How many characters long is the longest title?"
+    answer = reports.count_longest_title(file_name)
+    my_pretty_print(question, answer)
+
+
+def print_get_date_avg(file_name):
+    question = "What is the average of the release dates?"
+    answer = reports.get_date_avg(file_name)
+    my_pretty_print(question, answer)
+
+
+def print_get_game(file_name, title):
+    question = "What properties has a game?"
+    answer = reports.get_game(file_name, title)
+    my_pretty_print(question, answer)
+
+
+def print_count_grouped_by_genre(file_name):
+    question = "How many games are there grouped by genre?"
+    answer = reports.count_grouped_by_genre(file_name)
+    my_pretty_print(question, answer)
+
+
+def print_get_date_ordered(file_name):
+    question = "What is the date ordered list of the games?"
+    answer = reports.get_date_ordered(file_name)
+    my_pretty_print(question, answer)
+
+
+def my_pretty_print(question, answer):
+    """Formated output. Prints a different answer output according the type of the answer"""
+    answer_printers = {
+                       "type(answer) == list": "print()\n" + "pprint.pprint(answer, indent=40, width=55)",
+                       "type(answer) == bool": "print('{:36}{}'.format(' ', answer))",
+                       "type(answer) == dict": "print()\n" + "pprint_sorted_dict(answer, 35)",
+                       "type(answer) == float": "print(' {:^75.3f}'.format(answer))",
+                       "type(answer) not in (list, bool, dict, float)": "print(' {:^75}'.format(answer))",
+                       }
+    print("-"*85+"\nQuestion:{:^75}\nAnswer:".format(question), end="")
+    for key in answer_printers:
+        if eval(key):
+            exec(answer_printers[key])
+
+
+def pprint_sorted_dict(_dict, indent=0):
+    """Returns a sorted dictionary like pprint, but pprint does not have key=str.lower"""
+    if indent == 0:
+        end = ""
+    else:
+        end = "\n"
+    sorted_keys = sorted([i for i in _dict], key=str.lower)
+    print("{" + " "*(indent-1) + "'" + sorted_keys[0] + "'" + ": " + str(_dict[sorted_keys[0]]) + ",", end=end)
+    for index in range(1, len(sorted_keys) - 1):
+        print(" "*indent + "'" + sorted_keys[index] + "'" + ": " + str(_dict[sorted_keys[index]]) + ",", end=end)
+    print(" "*indent + "'" + sorted_keys[-1] + "'" + ": " + str(_dict[sorted_keys[-1]]) + "}")
+
+
+def main():
+    file_name, title = input_values()
+    print_get_most_played(file_name)
+    print_sum_sold(file_name)
+    print_get_selling_avg(file_name)
+    print_count_longest_title(file_name)
+    print_get_date_avg(file_name)
+    print_get_game(file_name, title)
+    print_count_grouped_by_genre(file_name)
+
+if __name__ == '__main__':
+    sys.exit(main())
