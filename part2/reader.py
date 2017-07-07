3e224b80c02012a2661b7ae1e4bff349d594b703
DanielKnoll <knoll.dani@gmail.com>
2017-07-08 00:33:03 +0200

    Refactored, less conversion in reports

--- part2/reader.py
+++ part2/reader.py
@@ -3,105 +3,132 @@ import datetime
 
 
 def read_file(file_name, simple_string_is_enough):
-    """Reader with input error handling. It can return two different type from the file"""
+    """Reader with input error handling. It can return two different type from the file
+       because for some questions a simple string is enough to work with."""
+
     try:
-        file = open(file_name)
+        with open(file_name) as file:
+            preferences_string = file.read()
     except FileNotFoundError:
         return "The game_stat.txt file is missing, or incorrect filename given."
     else:
-        error = check_errors_in_file(file, file_name)
+        error = check_errors_in_file(preferences_string, file_name)
         if error != "No errors":
             sys.exit(error)
 
         if simple_string_is_enough:
-            game_infos = convert_file_to_string(file)
+            return preferences_string
         else:
-            game_infos = convert_file_to_dict(file)
-        file.close()
-        return game_infos
+            preferences_dict = convert_string_to_dict(preferences_string)
+            return preferences_dict
 
 
-def check_errors_in_file(file, file_name):
+def check_errors_in_file(preferences_string, file_name):
     """Input error handling. Checks if the colunm number per line, sold copies, release date
     is not valid. If it finds any error exits the program with a message.
     """
-    for line_num, line in enumerate(file):
+    preferences_lines = preferences_string.splitlines()
+    for line_num, line in enumerate(preferences_lines):
         if line.count("\t") != 4:
             return "InputError: You have incorrect number of values in line " \
-                   + str(line_num) + " in " + file_name \
+                   + str(line_num + 1) + " in " + file_name \
                    + "\nExpected values: title->total copies sold->release date->genre->publisher↲"
 
-        current_line = line[:-1].split("\t")
+        line_lst = line.split("\t")
 
-        converted_sold_copy = check_conversion_error(current_line[1], line_num, file_name)
-        if type(converted_sold_copy) == str:
-            return converted_sold_copy
-        elif converted_sold_copy > 500:
-            return "InputError: You have too high second value for sold copies in line " \
-                   + str(line_num + 1) + " in " + file_name \
-                   + "\nAccording to Wikipedia not even Tetris reached 500M."
-        elif converted_sold_copy < 0.1:
-            return "InputError: Why even borher with niche games like " \
-                   + current_line[0] + " in line " + str(line_num + 1) + " in " + file_name \
-                   + "\nTake that line out! We are dealing with big guns here!"
+        for index in range(1, 3):
+            converted_input = check_conversion_error(line_lst[index], index, line_num, file_name)
+            is_error = check_value_error(converted_input, index, line_num, file_name, line_lst[0])
+            if type(is_error) == str:
+                return is_error
 
-        converted_release_date = check_conversion_error(current_line[2], line_num, file_name)
-        if type(converted_release_date) == str:
-            return converted_release_date
-        elif (len(str(converted_release_date)) != 4) or (str(converted_release_date)[0] not in ("1", "2") or (
-              converted_release_date < 0)):
-            return "InputError: You have a number at the third value in line " \
-                   + str(line_num + 1) + " in " + file_name \
-                   + "which is not a valid year."
-        elif converted_release_date < 1958:
-            return "InputError: You have an earlier game release date than Pong (1958) in line " \
-                   + str(line_num + 1) + " in " + file_name
-        elif converted_release_date > datetime.date.today().year:
-            return "InputError: You have a game which released in the future in line " \
-                   + str(line_num + 1) + " in " + file_name
-
-    file.seek(0)
     return "No errors"
 
 
-def check_conversion_error(lst_value_str, line_num, file_name):
+def check_conversion_error(value_str, index, line_num, file_name):
     """ Float and int convertion error handler in one function."""
     outputs = {
-               "copies": ["float(lst_value_str)", "second"],
-               "year": ["int(lst_value_str)", "third"],
+               "index == 1": ["float(value_str)", "second"],
+               "index == 2": ["int(value_str)", "third"],
               }
-    if (lst_value_str[0] in ("1", "2")) and (len(lst_value_str) >= 4) and ("." not in lst_value_str):
-        key = "year"
-    else:
-        key = "copies"
-    try:
-        convertable_value = eval(outputs[key][0])
-    except ValueError:
-        error_message = "You have letters/symbols in the " + outputs[key][1] \
-                        + " value at line " + str(line_num + 1) + " in " + file_name
-        return error_message
-    else:
-        return convertable_value
+    for key in outputs:
+        if eval(key):
+            try:
+                converted_value = eval(outputs[key][0])
+            except ValueError:
+                error_message = "You have letters/symbols in the " + outputs[key][1] \
+                                + " value at line " + str(line_num + 1) + " in " + file_name
+                return error_message
+            else:
+                return converted_value
 
 
-def convert_file_to_string(file):
-    """For some questions a simple string is enough to work with."""
-    game_infos_string = file.read()
-    return game_infos_string
+def check_value_error(value, index, line_num, file_name, title):
+    keep_order = 0
+    this_year = datetime.date.today().year
+    value_cheks = [
+            {"type(value) == str": "value"},
+            {"index == 2": "3"},
+            {  # This part is only for the sold copies
+             "value > 500": "'InputError: You have too high second value for sold copies in line ' + " \
+                            "str(line_num + 1) + ' in ' + file_name + " \
+                            "'\\nAccording to Wikipedia not even Tetris reached 500M.'",
+
+             "value < 0.1": "'InputError: Why even borher with niche games like ' + title + " \
+                            "' in line ' + str(line_num + 1) + ' in ' + file_name + " \
+                            "'\\nTake that line out! We are dealing with big guns here!'",
+            },
+            {"'No problems here'": "len(value_cheks)"},
+            {  # This part is only for release dates
+             "value < 1958": "'InputError: You have an earlier game release date than Pong (1958) in line ' + " \
+                             "str(line_num + 1) + ' in ' + file_name",
+             "value > this_year": "'InputError: You have a game which released in the future in line ' +"
+                                  "str(line_num + 1) + ' in ' + file_name",
+
+            },
+            ]
+
+    while keep_order < len(value_cheks):
+        for key in value_cheks[keep_order]:
+            if eval(key):
+                if not value_cheks[keep_order][key].isdigit():
+                    return eval(value_cheks[keep_order][key])
+                keep_order = eval(value_cheks[keep_order][key])
+
+        keep_order += 1
 
 
-def convert_file_to_dict(file):
-    """Returns a dictionary with same properties in one list. Information about a single
-    game will be stored in the same list index under different keys.
+def convert_string_to_dict(preferences_string):
+    """Puts tabulator separated colunms from multiline string to a list. Stores the separate columns in dict
+    with key values stored in a list to keep the same order as in the string.
     """
-    game_infos_dict = {}
+    preferences_dict = {}
     info_categories = ["titles", "copies sold lst", "release dates", "genres", "publishers"]
     for key in info_categories:
-        game_infos_dict.update({key: []})
+        preferences_dict.update({key: []})
 
-    for line in file:
-        one_game = line[:-1].split("\t")
+    preferences_lines = preferences_string.splitlines()
+    for line in preferences_lines:
+        one_game = line.split("\t")
         for index, value in enumerate(one_game):
-            game_infos_dict[info_categories[index]].append(value)
+            convert_nums = convert_type(value, index)
+            preferences_dict[info_categories[index]].append(convert_nums)
 
-    return game_infos_dict
+    return preferences_dict
+
+
+def convert_type(value_str, index):
+    try:
+        value_str.isdigit()
+    except AttributeError:
+        return "TypeError: use 'str' object"
+
+    conversions = {
+                   "1": "float(value_str)",
+                   "2": "int(value_str)",
+                  }
+
+    for key in conversions:
+        if index == eval(key):
+            return eval(conversions[key])
+    return value_str
--- part2/reports.py
+++ part2/reports.py
@@ -6,22 +6,20 @@ def get_most_played(file_name):
     corresponding value from dictionary's titles named list.
     """
     preferences_dict = reader.read_file(file_name, simple_string_is_enough=False)
-    sold_float = list(map(lambda x: float(x), preferences_dict["copies sold lst"]))
-    index = sold_float.index(max(sold_float))
+    index = preferences_dict["copies sold lst"].index(max(preferences_dict["copies sold lst"]))
     return preferences_dict["titles"][index]
 
 
 def sum_sold(file_name):
     """Returns the summary of dictionayr's copies sold lst named list"""
     preferences_dict = reader.read_file(file_name, simple_string_is_enough=False)
-    return sum(map(lambda x: float(x), preferences_dict["copies sold lst"]))
+    return sum(preferences_dict["copies sold lst"])
 
 
 def get_selling_avg(file_name):
     """Returns the average of dictionayr's copies sold lst named list"""
     preferences_dict = reader.read_file(file_name, simple_string_is_enough=False)
-    sold_float_list = list(map(lambda x: float(x), preferences_dict["copies sold lst"]))
-    return sum(sold_float_list) / len(sold_float_list)
+    return sum(preferences_dict["copies sold lst"]) / len(preferences_dict["copies sold lst"])
 
 
 def count_longest_title(file_name):
@@ -33,28 +31,19 @@ def count_longest_title(file_name):
 def get_date_avg(file_name):
     """Returns the average of dictionayr's release dates named list"""
     preferences_dict = reader.read_file(file_name, simple_string_is_enough=False)
-    dates_float_list = list(map(lambda x: int(x), preferences_dict["release dates"]))
-    date_avg = sum(dates_float_list) / len(dates_float_list)
+    date_avg = sum(preferences_dict["release dates"]) / len(preferences_dict["release dates"])
     return round(date_avg)
 
 
 def get_game(file_name, title):
     """Returns each properties of the given title in a list"""
-    preferences_str = reader.read_file(file_name, simple_string_is_enough=True)
-    pref_lst = preferences_str.splitlines()
-    for i, line in enumerate(pref_lst):
-        if title in line:
-            if title == (line.split("\t"))[0]:
-                break
-    if i+1 == len(pref_lst) and title not in line:
-        return "No such game."
-    title_properties = line.split("\t")
+    preferences_dict = reader.read_file(file_name, simple_string_is_enough=False)
     try:
-        title_properties[1] = float(title_properties[1])
-        title_properties[2] = int(title_properties[2])
+        index = preferences_dict["titles"].index(title)
     except ValueError:
-        return "check_errors_in_file or check_conversion_error function(s) are removed"\
-               "or missing func call in reader.py"
+        return "No such game in the file."
+    preferences = ["titles", "copies sold lst", "release dates", "genres", "publishers"]
+    title_properties = [preferences_dict[key][index] for key in preferences]
     return title_properties
 
 
